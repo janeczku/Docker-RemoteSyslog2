8701e10e35c3da55f050d2d4d16fbfdd0e4a4ff0
janeczku <janeczku@yahoo.de>
2016-10-02 22:38:28 +0200

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # when no arguments are passed set options from environment
 # otherwise append all arguments to the remote_syslog command
 if [[ -z ${1} ]]; then
-  exec /remote_syslog/remote_syslog -D --dest-host=${SYSLOG_HOST} dest-port=${SYSLOG_PORT:-514} \
+  exec /remote_syslog/remote_syslog -D --dest-host=${SYSLOG_HOST} --dest-port=${SYSLOG_PORT:-514} \
   	--tcp=${SYSLOG_TCP:-false} --tls=${SYSLOG_TLS:-false} --facility=${SYSLOG_FACILITY:-user} \
   	--severity=${SYSLOG_SEVERITY:-notice} --hostname=${SYSLOG_HOSTNAME} ${SYSLOG_FILES}
 else
